36899c4b0cbecd71889fc234e98712b4f63425a5
NVS Abhilash <nvs232@gmail.com>
2018-06-13 00:42:05 +0530

    Update main.py to work with new file structure.

--- main.py
+++ main.py
@@ -2,22 +2,22 @@
 # 2. Make the double_metaphone HASH
 # 3. Combine the results
 
-import bk_tree as bt
-import double_metaphone as dm
-import levenshtein_distance as ld
+from correctme import bk_tree as bt
+from correctme import double_metaphone as dm
+from correctme import levenshtein_distance as ld
 import fuzzy
 
 def initializeApp ():
-    tree = bt.BKTree(ld.get_edit_distance, bt.dict_words('../google-10000-english-no-swears.txt'))
-    meta_dict = dm.load_metaphone_dictionary('../google-10000-english-no-swears.txt')
+    tree = bt.BKTree(ld.get_edit_distance, bt.dict_words('data/google-10000-english-no-swears.txt'))
+    meta_dict = dm.load_metaphone_dictionary('data/google-10000-english-no-swears.txt')
 
     return [tree, meta_dict]
 
 def getSuggestion (word, tree, meta_dict):
     dmeta = fuzzy.DMetaphone()
-    
+
     wordsList = tree.query(word, 1)
-    
+
     wordsList1 = []
     wordsList2 = []
     # for removing the edit distance value present in wordList
@@ -42,13 +42,13 @@ def getSuggestion (word, tree, meta_dict):
 
     # Find intersection of the two list
     wordsList3 = list (set(wordsList1) & set(wordsList2))
-    
+
     return [wordsList1, wordsList2, wordsList3]
 
 def main():
     tree, meta_dict = initializeApp()
     wordsList1, wordsList2, wordsList3 = getSuggestion ('more', tree, meta_dict)
-    
+
     print (wordsList1, wordsList2, wordsList3)
 
 if __name__ == "__main__":
